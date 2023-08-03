551960d451a9ac1fb9bfc670f0640050ef987382
LFF5644 (LFF-MiniBook) <meerschweinchen5644@gmail.com>
2023-08-03 17:45:19 +0200

    modifyed

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-date;
+
+echo "$ sudo usermod -aG video $USER";
+sudo usermod -aG video $USER;
+echo "";
+
 echo "$ sudo apt update";
 sudo apt update;
+echo "";
+
 echo "$ sudo apt install fbset";
-sudo apt install fbset -y
+sudo apt install fbset -y;
+echo "";
+
 echo "$ npm run init";
 npm run init;
+echo "";
 echo "";
--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-date;
-
 echo "$ git pull";
 git pull;
+echo "";
 
 echo "$ npm update";
 npm update;
+echo "";
 
 echo "$ npm run init";
 npm run init;
-
+echo "";
 echo "";
